b254ba2c1c19385bac178d016e5170c71dd8290e
[Noelia Lopez Galiana] <[nologa@uoc.edu]>
2024-05-12 22:10:34 +0200

    actualización

---
+++ PEC4_Ej_Prac/ecommerce/src/app/app-bouquet-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bouquet-list',
+  templateUrl: './bouquet-list.component.html',
+  styleUrls: ['./bouquet-list.component.css']
+})
+export class BouquetListComponent {
+}
